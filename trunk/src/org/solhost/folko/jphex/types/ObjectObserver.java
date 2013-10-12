f4c7dfaa10b94b193f0e40fb0e214bebe5a6f98c
folke.will@gmail.com <folke.will@gmail.com@e9d6451a-8841-b7e5-4904-8293a139f7f8>
2013-10-12 19:43:19 +0000

    Remove death status and teleport to temple instead, as per issue #10

--- trunk/src/org/solhost/folko/jphex/types/ObjectObserver.java
+++ trunk/src/org/solhost/folko/jphex/types/ObjectObserver.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright (c) 2013 Folke Will <folke.will@gmail.com>
- * 
+ *
  * This file is part of JPhex.
- * 
+ *
  * JPhex is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * JPhex is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -31,6 +31,5 @@ public interface ObjectObserver {
     public void onChildAdded(Item container, Item child);
     public void onChildRemoved(Item container, Item child);
     public void onDeath(Mobile mob);
-    public void onResurrect(Mobile mob);
     public void onOpponentChanged(Mobile mob, Mobile victim);
 }
