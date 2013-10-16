2cdd32997d6903beb56cb0a8fb98e718c1f1a5d5
folke.will@gmail.com <folke.will@gmail.com@e9d6451a-8841-b7e5-4904-8293a139f7f8>
2013-10-16 19:38:26 +0000

    Small fix: don't run daylight timer twice

--- trunk/src/org/solhost/folko/jphex/DayNightCycle.java
+++ trunk/src/org/solhost/folko/jphex/DayNightCycle.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Folke Will <folke.will@gmail.com>
+ *
+ * This file is part of JPhex.
+ *
+ * JPhex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPhex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.solhost.folko.jphex;
 
 public class DayNightCycle {
@@ -41,7 +59,6 @@ public class DayNightCycle {
         boolean phaseChanged = (hour == MORNING || hour == FORENOON || hour == NOON || hour == AFTERNOON || hour == EVENING || hour == NIGHT);
         listener.onTimeChange(phaseChanged);
         hourTimer.reset();
-        TimerQueue.get().addTimer(hourTimer);
     }
 
     public boolean isMorning() {
